7df40c9fa0bb1afd3ba9b1e22a331d5f00cd2b36
Manish Kumar <manishh.kumar2@happiestminds.com>
2019-10-26 17:24:51 +0530

    first commit

---
+++ DSAndAlgorithm/DSAndAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DSAndAlgorithm
+//
+//  Created by Manish Kumar on 26/10/19.
+//  Copyright © 2019 Manish. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
